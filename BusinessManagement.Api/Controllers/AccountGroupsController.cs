2e63d9c07a72e727973547a9efb2ac1e0c667ce6
Arcan Caner Şenol <arcaner666@gmail.com>
2022-04-08 11:14:47 +0300

    updated

---
+++ BusinessManagement.Api/Controllers/AccountGroupsController.cs
@@ -0,0 +1,27 @@
+﻿using BusinessManagement.BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BusinessManagement.Api.Controllers;
+
+[Route("api/[controller]/")]
+[ApiController]
+public class AccountGroupsController : ControllerBase
+{
+    private readonly IAccountGroupBl _accountGroupBl;
+
+    public AccountGroupsController(
+        IAccountGroupBl accountGroupBl
+    )
+    {
+        _accountGroupBl = accountGroupBl;
+    }
+
+    [HttpGet("getall")]
+    public IActionResult GetAll()
+    {
+        var result = _accountGroupBl.GetAll();
+        if (result.Success) 
+            return Ok(result);
+        return BadRequest(result);
+    }
+}

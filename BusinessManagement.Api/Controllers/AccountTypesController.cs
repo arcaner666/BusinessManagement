6024b6d03778ecb6f883b78a9f0f69693998b4d5
Arcan Caner Şenol <arcaner666@gmail.com>
2022-05-06 13:17:17 +0300

    updated

---
+++ BusinessManagement.Api/Controllers/AccountTypesController.cs
@@ -0,0 +1,36 @@
+﻿using BusinessManagement.BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BusinessManagement.Api.Controllers;
+
+[Route("api/[controller]/")]
+[ApiController]
+public class AccountTypesController : ControllerBase
+{
+    private readonly IAccountTypeBl _accountTypeBl;
+
+    public AccountTypesController(
+        IAccountTypeBl accountTypeBl
+    )
+    {
+        _accountTypeBl = accountTypeBl;
+    }
+
+    [HttpGet("getall")]
+    public IActionResult GetAll()
+    {
+        var result = _accountTypeBl.GetAll();
+        if (result.Success) 
+            return Ok(result);
+        return BadRequest(result);
+    }
+
+    [HttpGet("getbyaccountgroupname")]
+    public IActionResult GetByAccountTypeName(string accountTypeName)
+    {
+        var result = _accountTypeBl.GetByAccountTypeName(accountTypeName);
+        if (result.Success)
+            return Ok(result);
+        return BadRequest(result);
+    }
+}
